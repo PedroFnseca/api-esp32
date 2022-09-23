a5057798a6a31facbc50c39b89cc594e82bd9959
Pedro Fonseca <97262778+pedroFnseca@users.noreply.github.com>
2022-09-23 20:59:32 -0300

    Alterando nome do banco

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -5,7 +5,7 @@ async function connect(){
     const DB_HOST = process.env.DB_HOST || 'localhost'
     const DB_USER = process.env.DB_USER || 'root'
     const DB_PASS = process.env.DB_PASS || ''
-    const DB_NAME = process.env.DB_NAME || 'test'
+    const DB_NAME = process.env.DB_NAME || 'bd_esp32'
 
     const connection = await mysql.createConnection({
         host: DB_HOST,
